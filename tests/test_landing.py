8115cd7a83b8d7c29742946719f0582874f0d354
Mikel Solabarrieta <mikelsr@streamsets.com>
2022-01-07 00:49:42 +0100

    Correctly configure test environment

--- tests/test_landing.py
+++ tests/test_landing.py
@@ -6,14 +6,12 @@ from zones.landing.fileresource import FileDataSource
 from zones.landing.webresource import WebResourceDataSource
 
 
-@pytest.fixture
-def data_sources() -> List[DataSource]:
-    return [
-        FileDataSource(filepath="/Users/mikel/Code/github.com/mikelsr/adsdb/data/Population-EstimatesData-1.csv"),
-        WebResourceDataSource(url="https://adsdb.mikel.xyz/Population-EstimatesData-2.csv"),
-    ]
+_test_data_sources = [
+    FileDataSource(filepath="/Users/mikel/Code/github.com/mikelsr/adsdb/data/Population-EstimatesData-1.csv"),
+    WebResourceDataSource(url="https://adsdb.mikel.xyz/Population-EstimatesData-2.csv"),
+]
 
 
-def test_datasources_load(data_sources):
-    for data_source in data_sources:
-        assert data_source.load() is not None
+@pytest.mark.parametrize("data_source", _test_data_sources)
+def test_data_sources_load(data_source):
+    assert data_source.load() is not None
